0171756f1f2a6999654daab11d13db553d99031a
Dip Singha <dipsingha2@gmail.com>
2023-04-13 20:19:38 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import NavMenu from "./components/NavMenu";
-import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <NavMenu />
-      </BrowserRouter>
+    <div className="">
+      <NavMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,14 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { MdOutlineMenu, MdClose } from "react-icons/md";
 
 function NavMenu() {
+  const [showNav, setShowNav] = useState(false);
+
+  const handleClick = () => {
+    setShowNav(!showNav);
+  };
+
   return (
-    <div>
-      <ul className="flex justify-center items-center gap-10 py-5">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/project">Project</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+    <div className="text-white/50 z-50  fixed  w-full bg-neutral-900">
+      <div
+        onClick={handleClick}
+        className={
+          !showNav
+            ? "mobile-menu-icon absolute right-[1rem] top-[1rem] cursor-pointer outline-none w-[4rem] sm:hidden"
+            : "hidden"
+        }
+      >
+        <MdOutlineMenu />
+      </div>
+
+      <ul
+        className={
+          !showNav
+            ? "sm:flex sm:justify-center sm:items-center sm:gap-10 sm:py-5 hidden"
+            : "flex flex-col justify-center items-center gap-10 py-5"
+        }
+      >
+        <div
+          onClick={handleClick}
+          className={
+            showNav
+              ? "close-icon absolute right-[1rem]  top-[1rem] cursor-pointer w-[4rem]  sm:{}"
+              : "hidden"
+          }
+        >
+          <MdClose />
+        </div>
+        <li className="">
+          <NavLink to="/" className=" hover:text-white duration-300">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className=" hover:text-white duration-300">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects" className=" hover:text-white duration-300">
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className=" hover:text-white duration-300">
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
